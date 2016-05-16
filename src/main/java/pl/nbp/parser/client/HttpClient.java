f72ce022a6c751963668774825e87e411009ba32
Maciej Sroka <maciej.sroka2@gmail.com>
2016-05-16 22:49:26 +0200

    Added HttpClient

---
+++ src/main/java/pl/nbp/parser/client/HttpClient.java
@@ -0,0 +1,11 @@
+package pl.nbp.parser.client;
+
+import com.jcabi.http.request.JdkRequest;
+
+import java.io.IOException;
+
+public class HttpClient {
+    public static String getStringFromUri(String uri) throws IOException {
+        return new String(new JdkRequest(uri).fetch().binary());
+    }
+}
