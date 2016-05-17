5081596b2172bb4e2c6fcfd7a128e2a5bd101b62
Maciej Sroka <maciej.sroka2@gmail.com>
2016-05-17 22:25:33 +0200

    Added ExchangeData resolver (class that contains logic of resolving calculations from NBP API)

---
+++ src/main/java/pl/nbp/parser/exchangeRate/resolver/ExchangeRateDataResolver.java
@@ -0,0 +1,62 @@
+package pl.nbp.parser.exchangeRate.resolver;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import pl.nbp.parser.client.NbpClient;
+import pl.nbp.parser.exchangeRate.ExchangeRate;
+import pl.nbp.parser.exchangeRate.operations.ExchangeRateCalculator;
+import pl.nbp.parser.exchangeRate.operations.ExchangeRateFilter;
+import pl.nbp.parser.exchangeRate.parser.ExchangeRateParser;
+import pl.nbp.parser.params.Params;
+
+import javax.xml.bind.JAXBException;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ExchangeRateDataResolver {
+    static final Logger LOGGER = LoggerFactory.getLogger(ExchangeRateDataResolver.class);
+
+    public static ExchangeRateData resolve(Params params) throws IOException {
+        List<String> exchangeDateFileNames = NbpClient.getExchangeRatesFilesBetweenDates(
+                params.getDateFrom(),
+                params.getDateTo()
+        );
+
+        List<ExchangeRate> exchangeRates = exchangeDateFileNames.stream().flatMap(el -> {
+            try {
+                return ExchangeRateParser.parseToList(NbpClient.getFileContent(el)).stream();
+            } catch (IOException e) {
+                LOGGER.error("IO error during parsing of '{}' file", el);
+            } catch (JAXBException e) {
+                LOGGER.error("XML parse error during parsing of '{}' file", el);
+            }
+            return null;
+        }).collect(Collectors.toList());
+
+        List<ExchangeRate> filteredExchangeRates = ExchangeRateFilter.filterByCurrencyCode(exchangeRates, params.getCurrency());
+
+        return new ExchangeRateData(
+                ExchangeRateCalculator.calculateAverageRateBuy(filteredExchangeRates),
+                ExchangeRateCalculator.calculateStandardDeviationOfRateSell(filteredExchangeRates)
+        );
+    }
+
+    public static class ExchangeRateData {
+        private final Double buyAverage;
+        private final Double sellStandardDeviation;
+
+        public ExchangeRateData(Double buyAverage, Double sellStandardDeviation) {
+            this.buyAverage = buyAverage;
+            this.sellStandardDeviation = sellStandardDeviation;
+        }
+
+        public Double getBuyAverage() {
+            return buyAverage;
+        }
+
+        public Double getSellStandardDeviation() {
+            return sellStandardDeviation;
+        }
+    }
+}
---
+++ src/test/java/pl/nbp/parser/exchangeRate/ExchangeRateDataResolerTest.java
@@ -0,0 +1,27 @@
+package pl.nbp.parser.exchangeRate;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import pl.nbp.parser.exchangeRate.resolver.ExchangeRateDataResolver;
+import pl.nbp.parser.params.Params;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ExchangeRateDataResolerTest {
+    @Test
+    public void shouldResturnExchangeRateDataForGivenParams() throws IOException {
+        Params params = new Params("USD", LocalDate.of(2016, 1, 2), LocalDate.of(2016, 1, 8));
+
+        ExchangeRateDataResolver.ExchangeRateData exchangeRateData = ExchangeRateDataResolver.resolve(params);
+        assertThat(
+                exchangeRateData,
+                Matchers.allOf(
+                        Matchers.hasProperty("buyAverage", Matchers.closeTo(3.95209, 0.00001)),
+                        Matchers.hasProperty("sellStandardDeviation", Matchers.closeTo(0.0195, 0.0001))
+                )
+        );
+    }
+}
