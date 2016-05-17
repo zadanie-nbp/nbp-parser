bb4d6e523bf84d357876e6fa0f03c06ea5b0b1e5
Maciej Sroka <maciej.sroka2@gmail.com>
2016-05-17 22:25:52 +0200

    Added logic to MainClass

--- src/main/java/pl/nbp/parser/MainClass.java
+++ src/main/java/pl/nbp/parser/MainClass.java
@@ -1,7 +1,25 @@
 package pl.nbp.parser;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import pl.nbp.parser.exchangeRate.resolver.ExchangeRateDataResolver;
+import pl.nbp.parser.params.Params;
+import pl.nbp.parser.params.ParamsParser;
+
+import java.io.IOException;
+
 public class MainClass {
+    static final Logger LOGGER = LoggerFactory.getLogger(ExchangeRateDataResolver.class);
 
     public static void main(String[] args) {
+        Params params = ParamsParser.parseArguments(args[0], args[1], args[2]);
+        try {
+            ExchangeRateDataResolver.ExchangeRateData exchangeRateData = ExchangeRateDataResolver.resolve(params);
+            System.out.println(exchangeRateData.getBuyAverage());
+            System.out.println(exchangeRateData.getSellStandardDeviation());
+        } catch (IOException e) {
+            LOGGER.error("Error while parsing params");
+        }
+
     }
 }
